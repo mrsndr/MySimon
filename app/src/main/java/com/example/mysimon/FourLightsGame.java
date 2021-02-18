48dc2c42045f4d92ada4a79fa5e41876996647d7
mrsndr <jason.sndr@gmail.com>
2021-02-17 23:12:39 -0700

    Added some initial framework to the game and some partial framework to the ImageBlinker

--- app/src/main/java/com/example/mysimon/FourLightsGame.java
+++ app/src/main/java/com/example/mysimon/FourLightsGame.java
@@ -8,11 +8,14 @@ import android.widget.TextView;
 
 public class FourLightsGame extends AppCompatActivity {
 
-    private ImageView B1, B2, B3, B4;
-    private Integer gameState;  /** 0-init 1-Watch 2-Play 3-End **/
-    private TextView stateDisplay;
 
+    private int gameState;  /** 0-init 1-Watch 2-Play 3-End **/
+    // private Integer currentLevel;   /** Used to communicate how many blinks or button presses to do/expect **/
+    private TextView stateDisplay;  /** This is the text view that tells the user what is happening **/
+    private ImageView B1, B2, B3, B4;   /** quick names for the buttons **/
 
+    private int[] correctSequence;  /** This will be the sequence that the game displays and expects from the player
+                                        The first position [0] will be the level number **/
 
 
     @Override
@@ -20,17 +23,20 @@ public class FourLightsGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four_lights_game);
 
+        /** Initialize the array for later use **/
+        correctSequence = new int[11];  /** Positions 1-10 for this game, position 0 is is the current level **/
+
         /** Get the display textview for game mode instructions **/
          stateDisplay = findViewById(R.id.statetextView);
 
         /** Set the mode to initialization **/
-        setModeToInitialized();
+        setModeToReady();
 
 
     }
 
 
-    private void setModeToInitialized () {
+    private void setModeToReady () {
         /** Call this to reset for next round **/
         gameState = 0;
         stateDisplay.setText("Ready?");
--- app/src/main/java/com/example/mysimon/ImageBlinker.java
+++ app/src/main/java/com/example/mysimon/ImageBlinker.java
@@ -3,6 +3,8 @@ package com.example.mysimon;
 import android.os.CountDownTimer;
 
 public class ImageBlinker extends CountDownTimer {
+    private int[] correctSequence;
+    private boolean blinkTiming;    /** 0-Turn on 1-Turn off and next **/
     /**
      * @param millisInFuture    The number of millis in the future from the call
      *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
@@ -10,17 +12,32 @@ public class ImageBlinker extends CountDownTimer {
      * @param countDownInterval The interval along the way to receive
      *                          {@link #onTick(long)} callbacks.
      */
-    public ImageBlinker(long millisInFuture, long countDownInterval) {
-        super(millisInFuture, countDownInterval);
+    public ImageBlinker(long millisInFuture, long countDownInterval, int[] sequenceData) {
+        super(((millisInFuture * 2)*sequenceData[0]), countDownInterval);
+        blinkTiming = false;    /** first position **/
+        correctSequence = sequenceData;
     }
 
     @Override
     public void onTick(long millisUntilFinished) {
 
+
+
+        if (blinkTiming) {
+            /** turn off and set next **/
+
+        } else {
+            /** turn on **/
+
+        }
+
+        blinkTiming = !blinkTiming; /** Flip boolean **/
+
+
     }
 
     @Override
     public void onFinish() {
-
+        //myImage.setImageResource(R.drawable.ic_circle);
     }
 }
